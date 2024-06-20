2ae51156844cf70830a5193a38465817f94ad5fe
shiwa001 <oka@example.com>
2024-06-20 18:57:34 +0800

    Get data

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,30 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  css: ['~/assets/css/main.css'],
+
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+
   routeRules: {
     // prerender index route by default
     '/': { prerender: true },
+  },
+
+  modules: ["@nuxt/ui", "nuxt-vuefire"],
+  vuefire: {
+    config: {
+      apiKey: "AIzaSyDY7y2cLY4auoLgRHeQW_lo-X8-ivLpJo4",
+      authDomain: "undiksha-basisdata.firebaseapp.com",
+      projectId: "undiksha-basisdata",
+      storageBucket: "undiksha-basisdata.appspot.com",
+      messagingSenderId: "912868962374",
+      appId: "1:912868962374:web:46987e8c256e3c44c05a47",
+      measurementId: "G-ZHF1LS9JWR"
+    },
   },
 });
---
+++ pages/index.vue
@@ -0,0 +1,53 @@
+<script setup>
+import { addDoc } from 'firebase/firestore';
+import { Timestamp } from 'firebase/firestore'
+
+const { $modelsRef } = useNuxtApp();
+const todos = useCollection($modelsRef);
+console.log(todos);
+// const todos = useCollection(collection(db, 'todos'))
+function addData(){
+  try {
+    addDoc(models, {
+        name: 'Prise de la Bastille'
+      });
+
+   } catch (error) {
+    console.error('Error writing document: ', error)
+  }
+  
+};
+
+</script>
+
+
+<template >
+<div class="bg-gray-50">
+  <h1 class="text-3xl font-bold underline">
+      Todo App
+    </h1>
+  <button @click.prevent="addData()">Test</button>
+  <div class="playground container mx-auto flex mb-4">
+    <div class="tasks w-1/3 bg-sky-500 opacity-30">
+      <div class="task">
+        <p v-for="todo in todos" :key="todo.id">{{ todo.name }}</p>
+       
+      </div>
+    </div>
+    <div class="tasks w-1/3">
+    <div class="task">
+      <p>task1</p>
+      <p>task2</p>
+      <p>task3</p>
+    </div>
+    </div>
+    <div class="tasks w-1/3">
+    <div class="task">
+      <p>task1</p>
+      <p>task2</p>
+      <p>task3</p>
+    </div>
+    </div>
+  </div>
+</div>
+</template>
